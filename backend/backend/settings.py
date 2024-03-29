8cdb7562bc3ba6d59807c4728d252a0315092f95
Kumar Utsav <utsav1072@gmail.com>
2024-03-29 18:24:03 +0530

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-btey+***-vrg5a#r3li_bt)lechpuo+ln607mjl#-$l^@y+&98
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
